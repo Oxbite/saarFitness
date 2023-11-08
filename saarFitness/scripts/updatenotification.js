4927c5394500969ee54c682bb7174d98e182face
nishedh <restricted.karki@gmail.com>
2023-11-08 17:22:26 +0545

    notification synchronous

--- saarFitness/scripts/updatenotification.js
+++ saarFitness/scripts/updatenotification.js
@@ -1,61 +1,63 @@
-const knex = require('knex')
-const fs = require('fs')
+const knex = require('knex');
+const fs = require('fs').promises;
 
-const databaseFilePath = '../database/db.db'
+const databaseFilePath = '../database/db.db';
 const db = knex({
   client: 'sqlite3',
   connection: {
-    filename: databaseFilePath
+    filename: databaseFilePath,
   },
-  useNullAsDefault: true
-})
+  useNullAsDefault: true,
+});
 
-fs.readFile("./oxbite.txt","utf8", (err, data) => {
+(async () => {
+  try {
+    const data = await fs.readFile("./oxbite.txt", "utf8");
     console.log(data);
-})
 
-const filePath = "./last_updated.json"
-const today = new Date().toISOString().split("T")[0]; // Convert today's date to ISO format
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    return;
-  }
+    const filePath = "./last_updated.json";
+    const today = new Date().toISOString().split("T")[0];
 
-  let jsonData = JSON.parse(data);
-  if (jsonData.date == "") {
-    jsonData.date == today
-  }
-  const last_date = jsonData.date;
-  if (today <= new Date(jsonData)){
-    console.log("Already checked for ", today, new Date(jsonData))
-    return;
-  }
-  db('subscription')
-    .select('subscription.id as subscription_id', 'subscription.customer', 'customer.fname as customer_name', 'subscription.end_date')
-    .join('customer', 'subscription.customer', 'customer.id')
-    .where('subscription.end_date', '<', today).andWhere('subscription.end_date', '>', last_date)
-    .then(async (expiredSubscriptions) => {
-      // Check if there are any expired subscriptions
+    try {
+      const jsonData = JSON.parse(await fs.readFile(filePath, 'utf8'));
+
+      if (jsonData.date === "") {
+        jsonData.date = today;
+      }
+
+      const last_date = jsonData.date;
+
+      if (today <= new Date(jsonData.date)) {
+        console.log("Already checked for", today, new Date(jsonData.date));
+        return;
+      }
+
+      const expiredSubscriptions = await db('subscription')
+        .select('subscription.id as subscription_id', 'subscription.customer', 'customer.fname as customer_name', 'subscription.end_date')
+        .join('customer', 'subscription.customer', 'customer.id')
+        .where('subscription.end_date', '<', today)
+        .andWhere('subscription.end_date', '>', last_date);
+
       if (expiredSubscriptions.length > 0) {
-        // Create notifications for each expired subscription
         const notifications = expiredSubscriptions.map((subscription) => ({
           read: false,
           link: `account-settings/${subscription.customer}/`,
-          message: `${subscription.customer_name}s subscription ended on ${subscription.end_date}.`,
+          message: `${subscription.customer_name}'s subscription ended on ${subscription.end_date}.`,
         }));
-        await db('notification')
-          .insert(notifications)
-        db.destroy()
-        return;
+
+        await db('notification').insert(notifications);
+        db.destroy();
       }
-    })
-    .catch((error) => {
+
+      jsonData.date = today;
+      await fs.writeFile(filePath, JSON.stringify(jsonData));
+      console.log("new date writing");
+    } catch (error) {
       console.error('Error:', error);
-  });
-  jsonData.date = today
-  fs.writeFile(filePath, JSON.stringify(jsonData), ()=> {
-    console.log("new date writing")
-    return;
-  })
-});
+    }
+  } catch (error) {
+    console.error('Error reading the file:', error);
+  } finally {
+    db.destroy();
+  }
+})();
